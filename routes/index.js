d4ff07afca5869abb048d160a268dc2c32118ab5
JerryChan31 <331498453@qq.com>
2018-10-05 23:33:53 +0800

    test webhook 2

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'test-auto-update' });
+  res.render('index', { title: 'test-auto-update 2' });
 });
 
 module.exports = router;
