b35c92f638f368b27f7ae3bda316d386d938a4b2
JerryChan31 <331498453@qq.com>
2018-11-02 10:17:33 +0800

    test compression.

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const indexRouter = require('./routes/index');
 
-const mongoose = require('mongoose');
+const compression = require('compression');
+
 const config = require('./config');
 
 const app = express();
@@ -13,18 +14,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
 module.exports = app;
 
-connect();
-
-function connect() {
-  const options = { useNewUrlParser: true};
-  mongoose.connect(config.db, options).then(
-    () => { console.log("db connected")},
-    err => { console.log(err)} //retry?
-  );
-}
